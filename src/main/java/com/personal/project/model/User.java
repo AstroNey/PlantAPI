b9d2cbda42d1fbf4a9cc3ed00885ee2569437ca0
astroney <l.nicolas02@laposte.net>
2024-10-25 14:05:28 +0200

    Try to scan with sonar

--- src/main/java/com/personal/project/model/User.java
+++ src/main/java/com/personal/project/model/User.java
@@ -1,9 +1,22 @@
 package com.personal.project.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
 
     private String FirstName;
 
     private String LastName;
+
+    private String Email;
+
+    private String Phone;
 
 }
