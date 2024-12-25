3b7b01e3ff41095a6b7b40e8499218bdb1fefb62
Lorentin <lorentin.nicolas@iut-rodez.fr>
2024-12-25 18:51:47 +0100

    add getALL plants

--- src/main/java/com/personal/project/controller/PlantController.java
+++ src/main/java/com/personal/project/controller/PlantController.java
@@ -4,14 +4,19 @@ package com.personal.project.controller;
 import com.personal.project.model.Plant;
 import com.personal.project.services.PlantService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Plant controller.
  */
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class PlantController {
 
     /**
@@ -39,5 +44,17 @@ public class PlantController {
         return plantService.findPlantById(id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    /**
+     * Get all plants if data exist.
+     * @return Get all plants
+     */
+    @GetMapping("/plants")
+    public ResponseEntity<List<Plant>> getPlants() {
+        Iterable<Plant> iterablePlants = plantService.findPlants();
+        List<Plant> plants = new ArrayList<>();
+        iterablePlants.forEach(plants::add);
+        return !plants.isEmpty() ?  ResponseEntity.ok(plants) : ResponseEntity.notFound().build();
     }
 }
